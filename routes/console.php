e98a63cca64b159ac4e248d863cec06974757adb
AbdulRochim <52064688+AbdulRochimDev@users.noreply.github.com>
2025-09-23 00:34:24 +0700

    Add default console routes bootstrap

--- routes/console.php
+++ routes/console.php
@@ -1,3 +1,8 @@
 <?php
 
-require __DIR__.'/examples/console.php';
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
